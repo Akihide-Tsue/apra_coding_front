ebe18c51378120787b60a23bb568d47b8942892a
Akihide-Tsue <akihide.tsue@gmail.com>
2024-06-08 13:46:13 +0900

    初期状態

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
-```bash
-npm run dev
+# 面接用コーディング課題
 
+## 環境構築
+
+まずはクローンをお願いします。
+
+```bash
+git clone https://github.com/Akihide-Tsue/apra_coding_front.git
+```
+
+起動コマンド
+
+```bash
+yarn
+yarn dev
+```
+
+※ yarnのインストールが必要な場合は
+
+```bash
+npm install -g yarn
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser.
+
+## 課題内容
+
+xxxを参照し、APIから取得したデータを使用して同じUIを実装して下さい。  
+cssはおおまかな目測で問題ありません。
+
+チェックポイント
+1.UIが正しい順番で表示できているか  
+2.クリック時の遷移を正しく実装できているか
+
+補足
+1.xxxxで取得したidと一致するデータを一番上に表示して下さい。
+2.idはフェッチごとに変わります。
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,7 @@
-import Head from "next/head"
-import Image from "next/image"
-import styles from "@/styles/App.module.css"
-import { Inter } from "next/font/google"
-
 export default function Home() {
   return (
-    <main className={`${styles.main}`}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/pages/index.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-    </main>
-  )
+    <div style={{ minHeight: "100vh", padding: "40px" }}>
+      <div>ここに追記して下さい 🚀</div>
+    </div>
+  );
 }
